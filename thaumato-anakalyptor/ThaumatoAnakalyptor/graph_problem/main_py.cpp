0c99109336778a789475f43b9eb8103bb859217c
Julian Schilliger <schillij@gmail.com>
2025-04-14 11:08:35 +0200

    smaller graph

--- thaumato-anakalyptor/ThaumatoAnakalyptor/graph_problem/main_py.cpp
+++ thaumato-anakalyptor/ThaumatoAnakalyptor/graph_problem/main_py.cpp
@@ -791,7 +791,7 @@ class Solver {
         }
         void load_graph(std::string graph_path) {
             try {
-                graph = loadGraph(graph_path, 1); // old graph version as default for now, switch over to 1 when new graphs arrive
+                graph = loadGraph(graph_path, 0); // old graph version as default for now, switch over to 1 when new graphs arrive
             }
             catch (...) {
                 std::cout << "Error loading graph, trying to load old graph version." << std::endl;
