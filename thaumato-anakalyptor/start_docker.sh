67013da7a51cccce1554d230a154c48b60b349a7
Julian Schilliger <schillij@gmail.com>
2025-05-02 15:26:22 +0200

    launch docker with any username

--- thaumato-anakalyptor/start_docker.sh
+++ thaumato-anakalyptor/start_docker.sh
@@ -2,7 +2,7 @@
 # Filename: run_thaumato.sh
 
 # Change directory to the repository and update it
-cd /home/ubuntu/villa || { echo "Directory /home/ubuntu/villa not found!"; exit 1; }
+cd "$HOME/villa" || { echo "Directory $HOME/villa not found!"; exit 1; }
 git pull
 
 # Enable X11 access for Docker containers and root
@@ -11,10 +11,10 @@ xhost +local:root
 
 # Run the Docker container with GPU access, large shared memory, multiple volume mounts, and GUI support.
 sudo docker run --gpus all --shm-size=150g -it --rm \
-    -v /home/ubuntu/Desktop/scrolls:/scrolls \
-    -v /home/ubuntu/villa/thaumato-anakalyptor/:/workspace \
-    -v /home/ubuntu/Desktop/experiments:/workspace/experiments \
+    -v "$HOME/Desktop/scrolls:/scrolls" \
+    -v "$HOME/villa/thaumato-anakalyptor/:/workspace" \
+    -v "$HOME/Desktop/experiments:/workspace/experiments" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -e DISPLAY=$DISPLAY \
+    -e DISPLAY="$DISPLAY" \
     thaumato_image \
     bash -c "chmod +x compile_cpp.sh GraphLabeler.sh ; exec bash"
