b53ab38f75598e136db3f74110a38c44672b3440
Julian Schilliger <julian@scrollprize.org>
2025-05-24 11:25:06 -0700

    instancs to h5 process launch, test env

--- thaumato-anakalyptor/process_scroll.sh
+++ thaumato-anakalyptor/process_scroll.sh
@@ -147,11 +147,6 @@ if (( start_idx <= 6 )); then
 fi
 
 if (( start_idx <= 7 )); then
-  # Source the conda initialization script to define conda commands.
-  source "$(conda info --base)/etc/profile.d/conda.sh"
-  echo conda info --envs
-  conda activate thaumato
-  
   # python3 -m ThaumatoAnakalyptor.instances_to_h5 --input_dir /workspace/experiments/point_cloud_colorized_verso_subvolume_blocks --output_h5 /workspace/experiments/point_cloud_colorized_verso_subvolume_blocks_compact.h5 --threads 12
   retry_until_success \
     python3 -m ThaumatoAnakalyptor.instances_to_h5 \
