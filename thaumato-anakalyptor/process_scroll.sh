6efa80d8e449b7aed0fdff074b9e1e653e4d1e52
Julian Schilliger <julian@scrollprize.org>
2025-05-24 11:06:07 -0700

    instancs to h5 process launch, test env

--- thaumato-anakalyptor/process_scroll.sh
+++ thaumato-anakalyptor/process_scroll.sh
@@ -149,11 +149,7 @@ fi
 if (( start_idx <= 7 )); then
   # Source the conda initialization script to define conda commands.
   source "$(conda info --base)/etc/profile.d/conda.sh"
-  # Deactivate conda completely (ensure no conda environment, not even base, remains active)
-  while [[ -n "${CONDA_DEFAULT_ENV:-}" ]]; do
-    conda deactivate
-  done
-  echo "Conda completely deactivated."
+  conda activate thaumato
   
   # python3 -m ThaumatoAnakalyptor.instances_to_h5 --input_dir /workspace/experiments/point_cloud_colorized_verso_subvolume_blocks --output_h5 /workspace/experiments/point_cloud_colorized_verso_subvolume_blocks_compact.h5 --threads 12
   retry_until_success \
