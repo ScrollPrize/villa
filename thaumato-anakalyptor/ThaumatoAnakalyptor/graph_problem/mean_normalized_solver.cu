94d4b75323fd19d72986ca7435f0081aeb1ee035
Julian Schilliger <schillij@gmail.com>
2025-06-16 15:39:35 +0200

    less strict triangle pruning

--- thaumato-anakalyptor/ThaumatoAnakalyptor/graph_problem/mean_normalized_solver.cu
+++ thaumato-anakalyptor/ThaumatoAnakalyptor/graph_problem/mean_normalized_solver.cu
@@ -98,7 +98,7 @@ __global__ void update_nodes_kernel_f_star_step(Node* d_graph, size_t* d_valid_i
         // k_factor = fminf(-1.0f, fmaxf(1.0f, k_factor));
         float dist_factor = - error_k / node_dist;
         float dist_f_sign = dist_factor > 0.0f ? 1.0f : -1.0f;
-        float abs_dist_factor = fabsf(fmaxf(fabsf(dist_factor), 0.001f) - 0.30f);
+        float abs_dist_factor = fabsf(fmaxf(fabsf(dist_factor), 0.001f) - 0.50f);
         dist_factor = dist_f_sign * abs_dist_factor * abs_dist_factor * abs_dist_factor;
         float step_loss = expf(-(k_diff * k_diff) / (2.0f * step_sigma * step_sigma));
         // step_loss = 1.0f; // only for testing, to make scroll windings straight in the end use the bell curve
--- thaumato-anakalyptor/ThaumatoAnakalyptor/pointcloud_mesh_refinement.py
+++ thaumato-anakalyptor/ThaumatoAnakalyptor/pointcloud_mesh_refinement.py
@@ -2486,7 +2486,7 @@ def mesh_uv_wraps(filtered_winding_path, output_dir, winding_direction=False, de
         mesh = clean_mesh(mesh,
                       longest_edge_pct=100,
                       area_pct=100,
-                      edge_length_thresh=250)
+                      edge_length_thresh=500)
 
         # Normalize UVs after cleaning and get natural image size
         res = normalize_uvs(mesh, verbose=True)
@@ -2680,7 +2680,7 @@ def mesh_uv_global(filtered_winding_path, output_path, winding_direction=False):
     mesh = clean_mesh(mesh,
                       longest_edge_pct=100,
                       area_pct=100,
-                      edge_length_thresh=250)
+                      edge_length_thresh=500)
 
     # Normalize UVs after cleaning and get natural image size
     mesh, natural_image_size = normalize_uvs(mesh, verbose=True)
