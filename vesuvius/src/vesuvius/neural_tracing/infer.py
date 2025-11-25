cd7e0c4767fed87b9525fd4fb97a5c526f1a3ab6
Sean Johnson <johnsonsr3243@gmail.com>
2025-11-25 15:28:04 -0600

    add tta

--- vesuvius/src/vesuvius/neural_tracing/infer.py
+++ vesuvius/src/vesuvius/neural_tracing/infer.py
@@ -5,9 +5,9 @@ import cc3d
 import torch
 import accelerate
 import numpy as np
-import torch.nn.functional as F
 
-from dataset import get_crop_from_volume, build_localiser, make_heatmaps
+from vesuvius.neural_tracing.dataset import get_crop_from_volume, build_localiser, make_heatmaps
+from vesuvius.models.run.tta import infer_with_tta
 
 
 class Inference:
@@ -18,10 +18,19 @@ class Inference:
             mixed_precision=config['mixed_precision'],
         )
 
+
         self.model = self.accelerator.prepare(model)
+        self.device = self.accelerator.device
         self.config = config
+        self.do_tta = config.get('do_tta', True)
+        self.tta_type = config.get('tta_type', "rotation")
+        self.tta_batched = bool(config.get('tta_batched', True))
+
+        if config.get('compile', True):
+            self.model.compile()
 
         self.model.eval()
+
 
         print(f"loading volume zarr {volume_zarr}...")
         ome_zarr = zarr.open_group(volume_zarr, mode='r')
@@ -38,19 +47,31 @@ class Inference:
         prev_v_heatmap = make_heatmaps([prev_v[None]], min_corner_zyx, crop_size) if prev_v is not None else torch.zeros([1, crop_size, crop_size, crop_size])
         prev_diag_heatmap = make_heatmaps([prev_diag[None]], min_corner_zyx, crop_size) if prev_diag is not None else torch.zeros([1, crop_size, crop_size, crop_size])
         inputs = torch.cat([
-            volume_crop[None, None].to(self.accelerator.device),
-            localiser[None, None].to(self.accelerator.device),
-            prev_u_heatmap[None].to(self.accelerator.device),
-            prev_v_heatmap[None].to(self.accelerator.device),
-            prev_diag_heatmap[None].to(self.accelerator.device),
+            volume_crop[None, None].to(self.device),
+            localiser[None, None].to(self.device),
+            prev_u_heatmap[None].to(self.device),
+            prev_v_heatmap[None].to(self.device),
+            prev_diag_heatmap[None].to(self.device),
         ], dim=1)
-        outputs = self.model(inputs)
-        logits = outputs['uv_heatmaps'] if isinstance(outputs, dict) else outputs
-        if isinstance(logits, (list, tuple)):
-            logits = logits[0]
-        logits = logits.squeeze(0).reshape(2, self.config['step_count'], crop_size, crop_size, crop_size)  # u/v, step, z, y, x
-        # TODO: test-time augmentation! as well as flip/rotate also consider very small spatial jitters etc
-        return F.sigmoid(logits), min_corner_zyx
+
+        def forward(model_inputs):
+            outputs = self.model(model_inputs)
+            logits = outputs['uv_heatmaps'] if isinstance(outputs, dict) else outputs
+            if isinstance(logits, (list, tuple)):
+                logits = logits[0]
+            return logits
+
+        def run_with_tta(model_inputs):
+            if self.do_tta:
+                return infer_with_tta(forward, model_inputs, self.tta_type, batched=self.tta_batched)
+            return forward(model_inputs)
+
+        with torch.no_grad():
+            logits = run_with_tta(inputs)
+            logits = logits.squeeze(0).reshape(2, self.config['step_count'], crop_size, crop_size, crop_size)  # u/v, step, z, y, x
+            probs = torch.sigmoid(logits)
+
+        return probs, min_corner_zyx
 
     def get_blob_coordinates(self, heatmap, min_corner_zyx, threshold=0.5, min_size=8):
         # Find up to four blobs of sufficient size; return their centroids in descending order of blob size
