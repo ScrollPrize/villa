8ab9a1b6b48a685cd395d88befeeab270d5c2fcb
Paul Henderson <paul@pmh47.net>
2025-12-15 19:14:07 +0000

    neural tracer: Evaluate multiple checkpoints in one invocation

--- vesuvius/src/vesuvius/neural_tracing/evaluate.py
+++ vesuvius/src/vesuvius/neural_tracing/evaluate.py
@@ -1,6 +1,6 @@
 import json
 import click
-from datetime import datetime
+from pathlib import Path
 
 from vesuvius.neural_tracing import trace as trace_module
 
@@ -15,18 +15,16 @@ def _parse_point(point):
 
 
 @click.command()
-@click.option("--points_path", type=click.Path(exists=True, dir_okay=False), required=True,help="Path to JSON file listing evaluation volumes and points")
-@click.option("--checkpoint_path", type=click.Path(exists=True), required=True, help="Path to checkpoint file or directory of checkpoints passed through to trace")
-@click.option('--steps_per_crop', type=int, default=1, help='Number of steps to take before sampling a new crop')
-@click.option('--max_size', type=int, default=60, show_default=True, help='Maximum patch side length (in vertices) for trace_patch_v4')
-def main(points_path, checkpoint_path, steps_per_crop, max_size):
+@click.option("--points_path", type=click.Path(exists=True, dir_okay=False), required=True, help="Path to JSON file listing evaluation volumes and points")
+@click.option("--checkpoint_path", "checkpoint_paths", type=click.Path(exists=True), multiple=True, required=True, help="Path to checkpoint file or directory of checkpoints (can be given multiple times)")
+@click.option("--steps_per_crop", type=int, default=1, help="Number of steps to take before sampling a new crop")
+@click.option("--max_size", type=int, default=60, show_default=True, help="Maximum patch side length (in vertices) for trace_patch_v4")
+def main(points_path, checkpoint_paths, steps_per_crop, max_size):
     """
     Run `trace.trace` for a collection of start points grouped by volume.
     """
     with open(points_path, "rt") as fp:
         volumes = json.load(fp)
-
-    eval_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
     for vol_idx, volume in enumerate(volumes):
         name = volume.get("name", f"volume_{vol_idx:03}")
@@ -46,36 +44,41 @@ def main(points_path, checkpoint_path, steps_per_crop, max_size):
 
         for pt_idx, point in enumerate(volume_points):
             x, y, z = _parse_point(point)
-            uuid = f"nt-eval_{eval_timestamp}_{x}-{y}-{z}"
-            click.echo(
-                f"Tracing volume={name} "
-                f"point_index={pt_idx} start_xyz=({x}, {y}, {z}) "
-                f"into {paths_dir}"
-            )
 
-            trace_module.trace.main(
-                args=[
-                    "--checkpoint_path",
-                    checkpoint_path,
-                    "--out_path",
-                    paths_dir,
-                    "--start_xyz",
-                    str(x),
-                    str(y),
-                    str(z),
-                    "--volume_zarr",
-                    volume_zarr,
-                    "--volume_scale",
-                    str(vol_scale),
-                    "--uuid",
-                    uuid,
-                    "--steps_per_crop",
-                    str(steps_per_crop),
-                    "--max_size",
-                    str(max_size),
-                ],
-                standalone_mode=False,
-            )
+            for checkpoint_path in checkpoint_paths:
+                ckpt = Path(checkpoint_path)
+                ckpt_dir = ckpt.parent.name if ckpt.is_file() else ckpt.name
+                uuid = f"nt-eval_{ckpt_dir}_{x}-{y}-{z}"
+                click.echo(
+                    f"Tracing volume={name} "
+                    f"point_index={pt_idx} start_xyz=({x}, {y}, {z}) "
+                    f"checkpoint={checkpoint_path} "
+                    f"into {paths_dir}"
+                )
+
+                trace_module.trace.main(
+                    args=[
+                        "--checkpoint_path",
+                        checkpoint_path,
+                        "--out_path",
+                        paths_dir,
+                        "--start_xyz",
+                        str(x),
+                        str(y),
+                        str(z),
+                        "--volume_zarr",
+                        volume_zarr,
+                        "--volume_scale",
+                        str(vol_scale),
+                        "--uuid",
+                        uuid,
+                        "--steps_per_crop",
+                        str(steps_per_crop),
+                        "--max_size",
+                        str(max_size),
+                    ],
+                    standalone_mode=False,
+                )
 
 
 if __name__ == "__main__":
