fd9503a4c7f8790e32f93e1a4ce04cf2d605fa81
Sean Johnson <johnsonsr3243@gmail.com>
2026-01-22 06:55:53 -0600

    change some defaults for copy in/out

--- volume-cartographer/apps/VC3D/CWindowContextMenu.cpp
+++ volume-cartographer/apps/VC3D/CWindowContextMenu.cpp
@@ -1115,12 +1115,12 @@ void CWindow::onNeighborCopyRequested(const QString& segmentId, bool copyOut)
     QJsonObject pass1Params;
     pass1Params["normal_grid_path"] = normalGridPath;
     pass1Params["neighbor_dir"] = copyOut ? QStringLiteral("out") : QStringLiteral("in");
-    pass1Params["neighbor_max_distance"] = 50;
+    pass1Params["neighbor_max_distance"] = 250;
     pass1Params["mode"] = QStringLiteral("gen_neighbor");
-    pass1Params["neighbor_min_clearance"] = 4;
+    pass1Params["neighbor_min_clearance"] = 2;
     pass1Params["neighbor_fill"] = true;
     pass1Params["neighbor_interp_window"] = 5;
-    pass1Params["generations"] = 2;
+    pass1Params["generations"] = 1;
     pass1Params["neighbor_spike_window"] = 2;
 
     auto pass1JsonFile = std::make_unique<QTemporaryFile>(QDir::temp().filePath("neighbor_copy_pass1_XXXXXX.json"));
