daf3eca8466ae5689176b4ff89bb6189ae0c060e
Hendrik Schilling <hendrik.schilling@posteo.de>
2026-01-27 12:21:12 +0100

    normals are getting there

--- vesuvius/src/vesuvius/exps_2d_model/opt_loss_dir.py
+++ vesuvius/src/vesuvius/exps_2d_model/opt_loss_dir.py
@@ -13,7 +13,11 @@ def direction_loss_map(*, res: fit_model.FitResult) -> tuple[torch.Tensor, torch
 	lm_v = 0.5 * (diff0_v * diff0_v + diff1_v * diff1_v)
 
 	# Horizontal-direction match using line-offset connections.
-	# Target is a right angle to the base UNet direction.
+	# The connection-derived direction should match the UNet direction encoding directly.
+	# Compare in base-mesh resolution to avoid mixing base-mesh geometry with HR-sampled direction fields.
+	unet_dir0_lr = F.interpolate(res.data_s.dir0, size=res.xy_lr.shape[-2:], mode="bilinear", align_corners=True)
+	unet_dir1_lr = F.interpolate(res.data_s.dir1, size=res.xy_lr.shape[-2:], mode="bilinear", align_corners=True)
+	mask_lr = F.interpolate(res.mask, size=res.xy_lr.shape[-2:], mode="nearest")
 	xy = res.xy_conn
 	left = xy[:, 0]
 	right = xy[:, 2]
@@ -26,16 +30,13 @@ def direction_loss_map(*, res: fit_model.FitResult) -> tuple[torch.Tensor, torch
 	inv_sqrt2 = 1.0 / (2.0 ** 0.5)
 	dir0_h = 0.5 + 0.5 * cos2
 	dir1_h = 0.5 + 0.5 * ((cos2 - sin2) * inv_sqrt2)
-	if dir0_h.shape[-2:] != res.data_s.dir0.shape[-2:]:
-		dir0_h = F.interpolate(dir0_h, size=res.data_s.dir0.shape[-2:], mode="bilinear", align_corners=True)
-		dir1_h = F.interpolate(dir1_h, size=res.data_s.dir0.shape[-2:], mode="bilinear", align_corners=True)
-
-	diff0_h = dir0_h - (1.0 - res.data_s.dir0)
-	diff1_h = dir1_h - (1.0 - res.data_s.dir1)
+	diff0_h = dir0_h - unet_dir0_lr
+	diff1_h = dir1_h - unet_dir1_lr
 	lm_h = 0.5 * (diff0_h * diff0_h + diff1_h * diff1_h)
 
-	lm = 0.5 * (lm_v + lm_h)
-	return lm * res.mask, res.mask
+	lm_v_lr = F.interpolate(lm_v, size=res.xy_lr.shape[-2:], mode="bilinear", align_corners=True)
+	lm = 0.5 * (lm_v_lr + lm_h)
+	return lm * mask_lr, mask_lr
 
 
 def direction_loss(*, res: fit_model.FitResult) -> torch.Tensor:
