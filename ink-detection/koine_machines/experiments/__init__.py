f7ace46df4e32a2f91b471d27ed6fbfa0e805cd1
Sean Johnson <johnsonsr3243@gmail.com>
2026-03-24 20:43:38 -0500

    remove more, move things around again

--- ink-detection/koine_machines/experiments/__init__.py
+++
@@ -1,9 +0,0 @@
-from ink.experiments.erm import EXPERIMENT as ERM_EXPERIMENT
-from ink.experiments.erm_patch_bundle import EXPERIMENT as ERM_PATCH_BUNDLE_EXPERIMENT
-from ink.experiments.erm_stitch_inference import EXPERIMENT as ERM_STITCH_INFERENCE_EXPERIMENT
-
-__all__ = [
-    "ERM_EXPERIMENT",
-    "ERM_PATCH_BUNDLE_EXPERIMENT",
-    "ERM_STITCH_INFERENCE_EXPERIMENT",
-]
