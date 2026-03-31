bbafcfd2eb2fa0925bd4e6ac380e944796a8b9a3
Sean Johnson <johnsonsr3243@gmail.com>
2026-03-30 21:13:55 -0500

    Update config_manager.py

--- vesuvius/src/vesuvius/models/configuration/config_manager.py
+++ vesuvius/src/vesuvius/models/configuration/config_manager.py
@@ -288,6 +288,7 @@ class ConfigManager:
         self.max_val_steps_per_epoch = int(self.tr_configs.get("max_val_steps_per_epoch", 50))
         self.train_num_dataloader_workers = int(self.tr_configs.get("num_dataloader_workers", 8))
         self.max_epoch = int(self.tr_configs.get("max_epoch", 5000))
+        self.early_stopping_patience = int(self.tr_configs.get("early_stopping_patience", 0))
         self.optimizer = self.tr_configs.get("optimizer", "SGD")
         self.initial_lr = float(self.tr_configs.get("initial_lr", 0.01))
         self.weight_decay = float(self.tr_configs.get("weight_decay", 0.00003))
