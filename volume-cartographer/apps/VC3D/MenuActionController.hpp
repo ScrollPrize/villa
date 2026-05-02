09d659c093d184aa9f68713e6ea93b8e6785ea1f
Sean Johnson <johnsonsr3243@gmail.com>
2026-05-02 00:11:24 -0500

    remove more unused

--- volume-cartographer/apps/VC3D/MenuActionController.hpp
+++ volume-cartographer/apps/VC3D/MenuActionController.hpp
@@ -20,7 +20,6 @@ class MenuActionController : public QObject
 
 public:
     static constexpr int kMaxRecentVolpkg = 10;
-    static constexpr int kMaxRecentRemote = 10;
 
     explicit MenuActionController(CWindow* window);
 
@@ -30,17 +29,12 @@ public:
     void refreshRecentMenu();
     void openVolpkgAt(const QString& path);
     void loadAttachedRemoteVolumesForCurrentPackage();
-    void openRemoteUrl(const QString& url, bool isRetry = false);
 
 private slots:
     void openVolpkg();
     void openRecentVolpkg();
     void openLocalZarr();
-    void openRemoteVolume();
-    void browseS3();
     void attachRemoteZarr();
-    void attachRemoteSegments();
-    void openRecentRemoteVolume();
     void showSettingsDialog();
     void showAboutDialog();
     void showKeybindings();
@@ -64,21 +58,7 @@ private:
     QStringList loadRecentRemoteUrls() const;
     void saveRecentRemoteUrls(const QStringList& urls);
     void updateRecentRemoteList(const QString& url);
-    void refreshRecentRemoteMenu();
-    void ensureRecentRemoteActions();
     void attachRemoteZarrUrl(const QString& url, bool persistEntry = true);
-    void openRemoteZarr(const std::string& httpsUrl, const vc::HttpAuth& auth, const std::string& cachePath);
-    void openRemoteScroll(const std::string& httpsUrl, const vc::HttpAuth& auth, const std::string& cachePath);
-    void promptAndLoadRemoteSegments(const vc::HttpAuth& auth, const std::string& cachePath);
-    // Non-prompting variant: attaches the supplied segments URL and triggers
-    // the same discovery + surface-caching pipeline as the prompting path.
-    // On success, persists (segUrl, zarrUrl) in QSettings so auto-open of
-    // the same remote zarr can skip the prompt next time. Pass the current
-    // remote volume's URL as zarrUrl; empty disables persistence.
-    void loadRemoteSegmentsWithUrl(const QString& segUrl,
-                                   const vc::HttpAuth& auth,
-                                   const std::string& cachePath,
-                                   const QString& zarrUrl);
     bool tryResolveRemoteAuth(const QString& url,
                               vc::HttpAuth* authOut,
                               bool allowPrompt,
@@ -97,16 +77,11 @@ private:
     QMenu* _selectionMenu{nullptr};
     QMenu* _helpMenu{nullptr};
     QMenu* _recentMenu{nullptr};
-    QMenu* _recentRemoteMenu{nullptr};
 
     QAction* _openAct{nullptr};
     QAction* _openLocalZarrAct{nullptr};
-    QAction* _openRemoteAct{nullptr};
     QAction* _attachRemoteZarrAct{nullptr};
-    QAction* _attachRemoteSegmentsAct{nullptr};
-    QAction* _browseS3Act{nullptr};
     std::array<QAction*, kMaxRecentVolpkg> _recentActs{};
-    std::array<QAction*, kMaxRecentRemote> _recentRemoteActs{};
     QAction* _settingsAct{nullptr};
     QAction* _exitAct{nullptr};
     QAction* _keybindsAct{nullptr};
@@ -120,8 +95,6 @@ private:
     QAction* _selectionClearAct{nullptr};
     QAction* _importObjAct{nullptr};
     QAction* _rotateSurfaceAct{nullptr};
-    int _remoteOpenAuthRetries{0};
-    int _remoteScrollAuthRetries{0};
 
     QPointer<QDialog> _keybindsDialog;
 };
