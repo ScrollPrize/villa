e8292931462eff5ca86178e86e989b0c38fa0dc6
Sean Johnson <johnsonsr3243@gmail.com>
2026-06-17 13:08:28 -0500

    overrides for fake viewer test thing

--- volume-cartographer/core/test/test_viewer_overlay_surface_primitives.cpp
+++ volume-cartographer/core/test/test_viewer_overlay_surface_primitives.cpp
@@ -111,6 +111,8 @@ public:
     uint64_t selectedPointId() const override { return 0; }
     uint64_t selectedCollectionId() const override { return 0; }
     bool isPointDragActive() const override { return false; }
+    bool isSameWrapAnnotationModeEnabled() const override { return false; }
+    double sameWrapAnnotationPolylineOpacity() const override { return 1.0; }
     const std::vector<ViewerOverlayControllerBase::PathPrimitive>& drawingPaths() const override { return paths_; }
 
     void setOverlayGroup(const std::string& key, const std::vector<QGraphicsItem*>& items) override
