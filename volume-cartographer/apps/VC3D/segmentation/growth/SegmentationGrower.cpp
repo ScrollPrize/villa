1cc014d9ff5dda0332cc5ce0aa9253069c87d904
Sean Johnson <johnsonsr3243@gmail.com>
2026-07-08 20:12:07 -0500

    and another

--- volume-cartographer/apps/VC3D/segmentation/growth/SegmentationGrower.cpp
+++ volume-cartographer/apps/VC3D/segmentation/growth/SegmentationGrower.cpp
@@ -449,7 +449,11 @@ std::string generateTimestampString()
     auto now = std::chrono::system_clock::now();
     auto time = std::chrono::system_clock::to_time_t(now);
     std::tm tm{};
+#if defined(_WIN32)
+    gmtime_s(&tm, &time);
+#else
     gmtime_r(&time, &tm);
+#endif
 
     char buffer[32];
     std::strftime(buffer, sizeof(buffer), "%Y-%m-%dT%H-%M-%S", &tm);
